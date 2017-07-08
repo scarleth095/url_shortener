5d5ceaca1731ec3d0ee4243f1844b413d811eed0
Scarleth Estevez <scarleth@bu.edu>
2017-07-08 02:08:21 -0400

    postgres fix

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170708012840) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "long_url",      null: false
     t.string   "short_url_key", null: false
