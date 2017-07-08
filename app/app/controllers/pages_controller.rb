8c0469682ecd88cb67a51b5a074be8ae6426064f
Scarleth Estevez <scarleth@bu.edu>
2017-07-07 23:15:10 -0400

    simple url set up

--- app/app/controllers/pages_controller.rb
+++ app/app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
     def home
+        @alert="none"
+        @records=Url.all
+        if params[:alert].present?
+            @alert=params[:alert]
+        end 
     end
     
     def url_direct
-        Urls.find_by(short_url_key: params[:short_url_key])
+        url_key=Url.find_by(short_url_key: params[:short_url_key])
+        if (url_key!=nil)
+            redirect_to url_key.long_url
+        else
+            redirect_to action: 'home', alert: 'Url not found'
+        end
     end
 end
--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Url.create(long_url: 'http://scarlethestevez.me/', short_url_key: 'hdk6vv9s')
